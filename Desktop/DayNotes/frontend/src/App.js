a9c27381bd5ef5c39ac917c8856061360ec1b983
Rafael Saraiva <rafael-srv@hotmail.com>
2022-07-29 21:22:18 -0300

    Implementação da edição de notas

--- Desktop/DayNotes/frontend/src/App.js
+++ Desktop/DayNotes/frontend/src/App.js
@@ -1,5 +1,11 @@
-import React, {useState} from 'react'
-import Header from './Header'
+import React, {useState, useEffect} from 'react'
+import api from './services/api'
+import './global.css'
+import './sidebar.css'
+import './app.css'
+import './main.css'
+import Notes from './Components/Notes'
+
 
 
 
@@ -7,23 +13,76 @@ import Header from './Header'
 function App() {
 
   
-  const [counter, setCounter] = useState(0);
 
-  function hadleSoma(){
-    setCounter(counter + 1)
+  const [title, setTitle] = useState('');
+  const [notes, setNotes] = useState('');
+  const [allNotes, setAllNotes] = useState([])
+
+  useEffect(() =>{
+
+    async function getAllNotes(){
+      const response = await api.get('/annotations', )
+
+      setAllNotes(response.data)
+    }
+
+    getAllNotes()
+  },[])
+
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    const response = await api.post('/annotations',{
+      title,
+      notes,
+      priority: false
+    })
+
+    setTitle("")
+    setNotes("")
+
   }
-
-  function hadleMenos(){
-    setCounter(counter - 1)
-  }
-
   return (
-    <>
-      <h1>{counter}</h1>
-      <button onClick={hadleMenos}>Subtração</button>
-      <button onClick={hadleSoma}>Soma</button>
-    </>  
-  );
+
+    <div id="app">
+      <aside>
+        <strong>Caderno de notas</strong>
+        
+        <form onSubmit={handleSubmit}>
+
+          <div className="input-block">
+            <label htmlFor="title">Título da Anotação</label>
+            <input 
+            required
+            value = {title}
+            onChange = {e => setTitle(e.target.value)}/>
+            
+            <div className="input-block">
+              <label htmlFor="nota">Anotações</label>
+              <textarea 
+              required
+              value = {notes}
+              onChange = {e => setNotes(e.target.value)}/>
+            </div>
+
+            <button type="submit">Salvar</button>
+          </div>
+
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          {allNotes.map(data => (
+            <Notes data={data}/> 
+          ))}
+          
+           
+        </ul>
+      </main>
+
+    </div>
+  )
 }
 
 export default App;
---
+++ Desktop/DayNotes/frontend/src/Components/Notes/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Notes({data}){
+
+    return (
+        <>
+        <li className='notepad-infos'>
+            <div>
+              <strong>{data.title}</strong>
+              <div>
+                x
+              </div>
+            </div>
+            <textarea defaultValue={data.notes}></textarea>
+            <span>!</span>
+        </li>
+
+          
+        </>
+
+    )   
+}
+
+export default Notes;
